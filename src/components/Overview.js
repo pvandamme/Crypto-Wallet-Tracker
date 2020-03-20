ace6b67243a64e0cbc3f3d1a2bd787f66ba52741
spitox <paul.vandamme@outlook.fr>
2020-03-20 17:51:27 +0100

    mini-css-extract-plugin configured

--- src/components/Overview.js
+++ src/components/Overview.js
@@ -1,7 +1,36 @@
-import React from 'react'
+import React, { Component } from 'react'
+import axios from 'axios'
 
-const Overview = () => {
-	return <p>This is the Overview Page !</p>
+export default class Overview extends Component {
+	state = {
+		loading: true
+	}
+
+	async componentDidMount() {
+		const options = {
+			url:
+				'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest',
+			method: 'GET',
+			header: {
+				'X-CMC_PRO_API_KEY': '8a928122-419b-4555-84ef-a73a731a753a'
+			},
+			proxy: {
+				host: '51.79.52.62',
+				port: 3128,
+				auth: {
+					username: 'mikeymike',
+					password: 'rapunz3l'
+				}
+			}
+		}
+		const response = await axios(options)
+		console.log(response)
+	}
+	render() {
+		return (
+			<div>
+				{this.state.loading ? <p>Loading...</p> : <p>Fetched !</p>}
+			</div>
+		)
+	}
 }
-
-export default Overview
--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,71 @@
 const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
-module.exports = {
-	entry: './src/index.js',
-	output: {
-		path: path.resolve('./public'),
-		filename: 'bundle.js'
-	},
-	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				exclude: /(node_modules|bower_components)/,
-				use: {
-					loader: 'babel-loader',
-					options: {
-						presets: ['@babel/preset-env', '@babel/preset-react'],
-						plugins: [
-							'@babel/plugin-proposal-object-rest-spread',
-							'@babel/plugin-proposal-class-properties'
-						]
+module.exports = (env) => {
+	const prod = env === 'production'
+
+	return {
+		entry: './src/index.js',
+		output: {
+			path: path.resolve('./public'),
+			filename: 'bundle.js'
+		},
+		module: {
+			rules: [
+				{
+					test: /\.js$/,
+					exclude: /(node_modules|bower_components)/,
+					use: {
+						loader: 'babel-loader',
+						options: {
+							presets: [
+								[
+									'@babel/preset-env',
+									{
+										targets: {
+											node: '10'
+										}
+									}
+								],
+								'@babel/preset-react'
+							],
+							plugins: [
+								'@babel/plugin-proposal-object-rest-spread',
+								'@babel/plugin-proposal-class-properties'
+							]
+						}
 					}
+				},
+				{
+					test: /\.s?css$/,
+					use: [
+						MiniCssExtractPlugin.loader,
+						'css-loader',
+						'sass-loader'
+					]
+				},
+				{
+					test: /\.(png|jpg|gif|svg)$/,
+					loader: 'url-loader'
 				}
+			]
+		},
+		plugins: [
+			new MiniCssExtractPlugin({
+				filename: 'styles.css'
+			})
+		],
+		devtool: prod ? 'source-map' : 'cheap-module-eval-source-map',
+		devServer: {
+			headers: {
+				'Access-Control-Allow-Origin': '*',
+				'Access-Control-Allow-Methods':
+					'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+				'Access-Control-Allow-Headers':
+					'X-Requested-With, content-type, Authorization'
 			},
-			{
-				test: /\.s?css$/,
-				use: ['style-loader', 'css-loader', 'sass-loader']
-			},
-			{
-				test: /\.(png|jpg|gif|svg)$/,
-				loader: 'url-loader'
-			}
-		]
-	},
-	devServer: {
-		contentBase: path.resolve('./public'),
-		historyApiFallback: true
+			contentBase: path.resolve('./public'),
+			historyApiFallback: true
+		}
 	}
 }
