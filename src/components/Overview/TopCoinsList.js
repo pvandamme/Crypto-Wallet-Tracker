f36b5ce78b70e4e2c1edfc5493923f0dd46f9654
spitox <paul.vandamme@outlook.fr>
2020-03-23 21:11:09 +0100

    add refresh button

--- src/components/Overview/TopCoinsList.js
+++ src/components/Overview/TopCoinsList.js
@@ -2,13 +2,20 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { getTopCoins } from '../../state/selectors/marketSelectors'
 import Coin from './Coin'
+import { fetchMarket } from '../../state/actions/marketActions'
+import { bindActionCreators } from 'redux'
 
 const TopCoinsList = (props) => {
 	const { topCoins } = props
 	const list = topCoins.map((coin) => <Coin coin={coin} key={coin.id} />)
 	return (
 		<div className="top-coins">
-			<h3>Market Overview</h3>
+			<div className="top-coins__title">
+				<h3>Market Overview</h3>
+				<button onClick={() => props.fetchMarket()}>
+					<i className="fas fa-sync-alt i"></i>
+				</button>
+			</div>
 			<ul className="top-coins__grid top-coins__legend">
 				<li># Name</li>
 				<li># Market Cap</li>
@@ -26,4 +33,8 @@ const mapStateToProps = (state) => {
 	}
 }
 
-export default connect(mapStateToProps)(TopCoinsList)
+const mapDispatchToProps = (dispatch) => {
+	return bindActionCreators({ fetchMarket }, dispatch)
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(TopCoinsList)
