110bb0c94148adb22dec2e80f3a29b53e21024f7
spitox <paul.vandamme@outlook.fr>
2020-03-26 10:02:47 +0100

    test animation

--- src/components/Overview/Coin.js
+++ src/components/Overview/Coin.js
@@ -1,26 +1,89 @@
 import React, { Component } from 'react'
 import { roundNumber, formatNumber } from '../../helpers/helpers'
+import { Spring } from 'react-spring/renderprops'
 
 class Coin extends Component {
+	state = {
+		details: false
+	}
+	handleChange = () => {
+		this.setState({ details: !this.state.details })
+	}
 	render() {
 		const { coin } = this.props
-		return (
-			<ul className="top-coins__grid">
-				<li>
-					<div className="top-coins__name">
-						<img src={coin.icon} alt="icon" />
-						{coin.name}
-					</div>
-				</li>
-				<li>${formatNumber(coin.mkCap)}</li>
-				<li className="top-coins__center">
-					${formatNumber(coin.price)}
-				</li>
-				<li className={coin.priceChange < 0 ? 'red' : 'green'}>
-					{roundNumber(coin.priceChange, 2)}%
-				</li>
-			</ul>
-		)
+		if (!this.state.details) {
+			return (
+				<ul
+					className="top-coins__grid"
+					onClick={() => this.handleChange()}>
+					<li>
+						<div className="top-coins__name">
+							<img src={coin.icon} alt="icon" />
+							{coin.name}
+						</div>
+					</li>
+					<li>${formatNumber(coin.mkCap)}</li>
+					<li className="top-coins__center">
+						${formatNumber(coin.price)}
+					</li>
+					<li className={coin.priceChange < 0 ? 'red' : 'green'}>
+						{roundNumber(coin.priceChange, 2)}%
+					</li>
+				</ul>
+			)
+		} else {
+			return (
+				<div>
+					<ul
+						className="top-coins__grid"
+						onClick={() => this.handleChange()}>
+						<li>
+							<div className="top-coins__name">
+								<img src={coin.icon} alt="icon" />
+								{coin.name}
+							</div>
+						</li>
+						<li>${formatNumber(coin.mkCap)}</li>
+						<li className="top-coins__center">
+							${formatNumber(coin.price)}
+						</li>
+						<li className={coin.priceChange < 0 ? 'red' : 'green'}>
+							{roundNumber(coin.priceChange, 2)}%
+						</li>
+					</ul>
+					<Spring
+						from={{ opacity: 0, marginTop: -60 }}
+						to={{ opacity: 1, marginTop: 0 }}>
+						{(props) => (
+							<div style={props}>
+								<ul
+									className="top-coins__grid slow"
+									onClick={() => this.handleChange()}>
+									<li>
+										<div className="top-coins__name">
+											<img src={coin.icon} alt="icon" />
+											CCCC
+										</div>
+									</li>
+									<li>${formatNumber(coin.mkCap)}</li>
+									<li className="top-coins__center">
+										${formatNumber(coin.price)}
+									</li>
+									<li
+										className={
+											coin.priceChange < 0
+												? 'red'
+												: 'green'
+										}>
+										{roundNumber(coin.priceChange, 2)}%
+									</li>
+								</ul>
+							</div>
+						)}
+					</Spring>
+				</div>
+			)
+		}
 	}
 }
 
