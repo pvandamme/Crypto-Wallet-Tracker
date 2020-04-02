fcb58a307250e6ead6327d3c2d9d12a27d6e2cf8
spitox <paul.vandamme@outlook.fr>
2020-04-02 22:26:38 +0200

    register form, css, validation

--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -1,60 +1,75 @@
-import React, { Component } from 'react'
+import React from 'react'
+import { useForm } from 'react-hook-form'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import { registerUser } from 'state/actions/authActions/registerActions'
+import { Link } from 'react-router-dom'
 
-class Register extends Component {
-	state = {
-		email: '',
-		password: '',
-		firstName: ''
-	}
-	handleChange = (e) => {
-		this.setState({
-			[e.target.id]: e.target.value
-		})
-	}
-	handleSubmit = async (e) => {
-		e.preventDefault()
-		this.props.registerUser(this.state.email, this.state.password)
-	}
-	render() {
-		return (
-			<div className="">
-				<form className="" onSubmit={this.handleSubmit}>
-					<h5 className="">Sign Up</h5>
-					<div className="">
-						<label htmlFor="email">Email</label>
-						<input
-							type="email"
-							id="email"
-							onChange={this.handleChange}
-						/>
-					</div>
-					<div className="">
-						<label htmlFor="password">Password</label>
-						<input
-							type="password"
-							id="password"
-							onChange={this.handleChange}
-						/>
-					</div>
-					<div className="">
-						<label htmlFor="firstName">First Name</label>
-						<input
-							type="text"
-							id="firstName"
-							onChange={this.handleChange}
-							required={true}
-						/>
-					</div>
-					<div className="">
-						<button className="">Sign Up</button>
-					</div>
-				</form>
-			</div>
-		)
-	}
+const Register = () => {
+	const { register, handleSubmit, errors } = useForm()
+	const onSubmit = (data) => console.log(data)
+	console.log(errors)
+	//handleSubmit = async (e) => {
+	//	e.preventDefault()
+	//this.props.registerUser(this.state.email, this.state.password)
+	//	}
+	return (
+		<form
+			className="form-wrapper"
+			autoComplete="off"
+			onSubmit={handleSubmit(onSubmit)}>
+			<h3 className="">Sign Up</h3>
+			<input
+				type="text"
+				placeholder="First name"
+				name="firstName"
+				ref={register({ required: true, maxLength: 80 })}
+			/>
+			{errors.firstName && <p>First name is required !</p>}
+			<input
+				type="text"
+				placeholder="Last name"
+				name="lastName"
+				ref={register({ required: true, maxLength: 100 })}
+			/>
+			{errors.lastName && <p>Last Name is required !</p>}
+			<input
+				type="text"
+				placeholder="Email"
+				name="email"
+				ref={register({
+					required: true,
+					pattern: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/
+				})}
+			/>
+			{errors.email && errors.email.type === 'required' && (
+				<p>Email is required !</p>
+			)}
+			{errors.email && errors.email.type === 'pattern' && (
+				<p>Please enter a valid email !</p>
+			)}
+			<input
+				type="password"
+				placeholder="Password"
+				name="password"
+				ref={register({
+					pattern: /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{6,})/,
+					required: true
+				})}
+			/>
+			{errors.password && errors.password.type === 'required' && (
+				<p className="form__reset-margin">Password is required !</p>
+			)}
+			{errors.password && errors.password.type === 'pattern' && (
+				<p>
+					Password must be at least 6 characters long contain a number
+					and an uppercase letter !
+				</p>
+			)}
+			<button type="submit">Register</button>
+			<Link to="/login">Already have an account ?</Link>
+		</form>
+	)
 }
 
 const mapDispatchToProps = (dispatch) => {
