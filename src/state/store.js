efc17196a2d77c28b9dc419b9d33b7938bb66953
spitox <paul.vandamme@outlook.fr>
2020-04-17 13:56:12 +0200

    fix compose issue

--- src/state/store.js
+++ src/state/store.js
@@ -1,4 +1,4 @@
-import { applyMiddleware, createStore } from 'redux'
+import { applyMiddleware, createStore, compose } from 'redux'
 import rootReducer from './reducers/rootReducer'
 import thunk from 'redux-thunk'
 import { verifyAuth } from './actions/authActions/verifyActions'
