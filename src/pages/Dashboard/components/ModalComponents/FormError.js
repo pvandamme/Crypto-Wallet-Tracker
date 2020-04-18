cfb588072eb2d081fab02514924f8ab9d261ad68
spitox <paul.vandamme@outlook.fr>
2020-04-18 16:19:07 +0200

    rename amount to quantity for error message

--- src/pages/Dashboard/components/ModalComponents/FormError.js
+++ src/pages/Dashboard/components/ModalComponents/FormError.js
@@ -4,10 +4,10 @@ const FormError = ({ errors }) => {
 	return (
 		<div className="form-error">
 			{errors.amount && errors.amount.type === 'required' && (
-				<p>Amount is required !</p>
+				<p>Quantity is required !</p>
 			)}
 			{errors.amount && errors.amount.type === 'pattern' && (
-				<p>Please enter a valid amount number !</p>
+				<p>Please enter a valid quantity number !</p>
 			)}
 			{errors.price && errors.price.type === 'required' && (
 				<p>Price is required !</p>
